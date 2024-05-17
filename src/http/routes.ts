3cf4540ee9c6ac11ab3687eaea91cec2e24404cd
Pedro Oliveira <Pedroaolive@gmail.com>
2024-05-17 17:24:57 -0300

    Feat: create new authenticate method for application

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./modules/user/controllers/register";
+import { autenticate } from "./modules/auth/controllers/authenticate";
 
 export const appRoutes = async (app: FastifyInstance) => {
-    app.post('/users', register)
-}
+  // auth routes
+  app.post("/sessions", autenticate);
+
+  // users routes
+  app.post("/users", register);
+};
