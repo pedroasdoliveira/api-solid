7986d2e7eada227987cc1fe4fb73053df96f5ef4
Pedro Oliveira <Pedroaolive@gmail.com>
2024-08-13 23:17:41 -0300

    test: add registe e2e test

--- src/http/controllers/user/test/register.spec.ts
+++ src/http/controllers/user/test/register.spec.ts
@@ -1,3 +1,24 @@
-import { test } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
 
-test("Teste", () => {});
+import { app } from "@/app";
+
+describe("Register (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("Should be able to register", async () => {
+    const response = await request(app.server).post("/users").send({
+      name: "Pedro Test",
+      email: "pedro_test@gmail.com",
+      password: "123456",
+    });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
